61effd89f03449f9ea1fc810f385b4173112be96
leofxs <asdfghjklzxcvbnm2023120231@gmail.com>
2025-12-22 15:54:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def getMongoClient(ConnectionURI = None):
         ConnectionURI = "mongodb+srv://gamblesite_db_user:VQKwxemda7DhocAi@gamblesite.ttpjfpf.mongodb.net/gamblesite?retryWrites=true&w=majority&appName=gamblesite"
     client = MongoClient(
         ConnectionURI,
-        serverSelectionTimeoutMS=10000,
+        serverSelectionTimeoutMS=20000,
         tls=True,
         tlsCAFile=certifi.where()
     )
