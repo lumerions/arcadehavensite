0337e87a7797edbd0a01ed94b78a4093adf36e43
cool705 <asdfghjklzxcvbnm20231@gmail.com>
2025-12-25 11:53:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,6 @@ app = FastAPI(
     version="1.0.0",
 )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 def getMongoClient(ConnectionURI = None):
     if not ConnectionURI:
