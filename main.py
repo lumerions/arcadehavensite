564783cfbee1a63bc90fa9fdfe3f8a079f5b5ac8
cool705 <asdfghjklzxcvbnm20231@gmail.com>
2025-12-25 11:12:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,6 @@ app = FastAPI(
     version="1.0.0",
 )
 
-os.makedirs("public", exist_ok=True)
-
 app.mount("/public", StaticFiles(directory="public"), name="public")
 
 def getMongoClient(ConnectionURI = None):
